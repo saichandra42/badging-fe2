83c32ca299e55a12854025337996482c1ce58c84
Sai Chandra <saichandra004@hotmail.com>
2020-09-07 13:31:09 +0530

    Recommendations loader

--- src/components/Recommendation/index.js
+++ src/components/Recommendation/index.js
@@ -6,6 +6,7 @@ import {
   Card,
   CardActionArea,
   CardMedia,
+  LinearProgress,
 } from "@material-ui/core";
 import { getRecommendations } from "../../API";
 import APP_CONSTANTS from "../../constants";
@@ -14,14 +15,18 @@ import { useHistory } from "react-router-dom";
 function Recommendation(props) {
   const history = useHistory();
   const [recommendations, setRecommendations] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { userId = 1 } = APP_CONSTANTS;
   useEffect(() => {
+    setLoading(true);
     getRecommendations(userId)
       .then((res) => {
         setRecommendations(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         setRecommendations([]);
+        setLoading(false);
       });
   }, []);
 
@@ -38,45 +43,52 @@ function Recommendation(props) {
         Recommended for you
       </Typography>
       <Grid container spacing={1}>
-        {recommendations.map((recommendation, i) => {
-          const { subdomain } = recommendation;
-          return (
-            <Grid item xs={3} key={i} style={{ display: "flex" }}>
-              <Card
-                style={{ width: "100%", cursor: "pointer" }}
-                onClick={() => gotoBadge(recommendation)}
-              >
-                <CardActionArea>
-                  <CardMedia
-                    image="https://assets.ey.com/content/dam/ey-sites/ey-com/en_ph/generic/logos/ey-white-logo.png"
-                    title={subdomain}
-                    style={{
-                      height: "200px",
-                      backgroundColor: "#000",
-                      backgroundSize: "contain",
-                    }}
-                  />
-                </CardActionArea>
-                <CardContent>
-                  <Typography
-                    variant="body1"
-                    color="textSecondary"
-                    gutterBottom
-                  >
-                    {subdomain}
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    color="textSecondary"
-                    gutterBottom
-                  >
-                    {recommendation.badge}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
+        {!loading &&
+          recommendations.map((recommendation, i) => {
+            const { subdomain } = recommendation;
+            return (
+              <Grid item xs={3} key={i} style={{ display: "flex" }}>
+                <Card
+                  style={{ width: "100%", cursor: "pointer" }}
+                  onClick={() => gotoBadge(recommendation)}
+                >
+                  <CardActionArea>
+                    <CardMedia
+                      image="https://assets.ey.com/content/dam/ey-sites/ey-com/en_ph/generic/logos/ey-white-logo.png"
+                      title={subdomain}
+                      style={{
+                        height: "200px",
+                        backgroundColor: "#000",
+                        backgroundSize: "contain",
+                      }}
+                    />
+                  </CardActionArea>
+                  <CardContent>
+                    <Typography
+                      variant="body1"
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      {subdomain}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      {recommendation.badge}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        {loading && (
+          <LinearProgress
+            color="secondary"
+            style={{ width: "100%", marginBottom: "2rem" }}
+          />
+        )}
       </Grid>
     </>
   );
